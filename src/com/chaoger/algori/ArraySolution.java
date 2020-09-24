5cdd1688d8dde378001fe1b9dd6cb65dcbc51272
leiyuchao <leiyuchao@corp.netease.com>
2020-09-24 11:14:42 +0800

    add 923 数组两题

--- src/com/chaoger/algori/ArraySolution.java
+++ src/com/chaoger/algori/ArraySolution.java
@@ -523,6 +523,27 @@ public class ArraySolution {
         return profit;
     }
 
+
+    /**
+     * 乘积最大子数组:给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组
+     * （该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+     *
+     * @param nums
+     * @return
+     */
+    public int maxProduct(int[] nums) {
+        //其实就是正负号的问题
+        int maxF = nums[0],minF = nums[0],ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int mx = maxF,mn = minF;
+            maxF = Math.max(nums[i],Math.max(nums[i]*mx,nums[i]*mn));
+            minF = Math.min(nums[i],Math.min(nums[i]*mx,nums[i]*mn));
+            ans = Math.max(ans,maxF);
+        }
+        return ans;
+
+    }
+
     /**
      *  多数元素:给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
      * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
@@ -544,24 +565,27 @@ public class ArraySolution {
 
 
     /**
-     * 乘积最大子数组:给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组
-     * （该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
-     *
+     * 除自身以外数组的乘积:给你一个长度为 n 的整数数组 nums，其中 n > 1，
+     * 返回输出数组 output ，其中 output[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积。
      * @param nums
      * @return
      */
-    public int maxProduct(int[] nums) {
-        //其实就是正负号的问题
-        int maxF = nums[0],minF = nums[0],ans = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            int mx = maxF,mn = minF;
-            maxF = Math.max(nums[i],Math.max(nums[i]*mx,nums[i]*mn));
-            minF = Math.min(nums[i],Math.min(nums[i]*mx,nums[i]*mn));
-            ans = Math.max(ans,maxF);
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        ans[0] =1;
+        for (int i = 1; i <n ; i++) {
+            ans[i] = nums[i-1]*ans[i-1];
+        }
+        int R = 1;
+        for (int i = n-1; i >=0 ; i--) {
+            ans[i]*=R;
+            R *= nums[i];
         }
         return ans;
-
     }
+
+
 
 
     /**
@@ -699,6 +723,31 @@ public class ArraySolution {
         return r - l < 0 ? 0 : r - l + 1;
 
     }
+
+
+    /**
+     * 任务调度器:给定一个用字符数组表示的 CPU 需要执行的任务列表。其中包含使用大写的 A - Z 字母表示的26 种不同种类的任务。
+     * 任务可以以任意顺序执行，并且每个任务都可以在 1 个单位时间内执行完。CPU 在任何一个单位时间内都可以执行一个任务，或者在待命状态。
+     * 然而，两个相同种类的任务之间必须有长度为 n 的冷却时间，因此至少有连续 n 个单位时间内 CPU 在执行不同的任务，或者在待命状态。
+     * 你需要计算完成所有任务所需要的最短时间。
+     * @param tasks
+     * @param n
+     * @return
+     */
+    public int leastInterval(char[] tasks, int n) {
+        int[] map = new int[26];
+        for (char c: tasks) {
+            map[c - 'A']++;
+        }
+        Arrays.sort(map);
+        int max_val = map[25] - 1, idle_slots = max_val * n;
+        for (int i = 0; i <= 24 ; i++) {
+            idle_slots -= Math.min(map[i], max_val);
+        }
+        return idle_slots > 0 ? idle_slots + tasks.length : tasks.length;
+
+    }
+
 
 
     public static void main(String[] args) {
