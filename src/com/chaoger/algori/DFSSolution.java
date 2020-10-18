81bc09b629b46319c362c63a71ac9c5bc0b23562
leiyuchao <leiyuchao@corp.netease.com>
2020-10-18 17:05:09 +0800

    10-20 dfs 2题

--- src/com/chaoger/algori/DFSSolution.java
+++ src/com/chaoger/algori/DFSSolution.java
@@ -1,5 +1,10 @@
 package com.chaoger.algori;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class DFSSolution {
 
     public class TreeNode {
@@ -9,6 +14,10 @@ public class DFSSolution {
         TreeNode(int x) { val = x; }
     }
 
+    public static void main(String[] args) {
+
+        System.out.println(decodeString("3[a]2[bc]"));
+    }
 
     /**
      * 二叉树的最大深度:给定一个二叉树，找出其最大深度。
@@ -51,6 +60,82 @@ public class DFSSolution {
         int robValue = root.val + left[1]+right[1];
         int skipValue = Math.max(left[0],left[1])+Math.max(right[0],right[1]);
         return new int[]{robValue,skipValue};
+
+    }
+
+    /**
+     * 二叉树展开为链表:给定一个二叉树，原地将它展开为一个单链表。
+     *
+     * @param root
+     */
+    public void flatten(TreeNode root) {
+        //解决方案也可以采用先序遍历的方式、不过时间复杂度为O(n)
+        while (root!=null){
+            while (root.left!=null){
+                TreeNode cur = root.left;
+                TreeNode pre = cur;
+                while (pre.right!=null){
+                    pre = pre.right;
+                }
+                pre.right = root.right;
+                root.left = null;
+                root.right = cur;
+            }
+            root = root.right;
+        }
+    }
+
+
+
+    /**
+     * 字符串解码：给定一个经过编码的字符串，返回它解码后的字符串。
+     * 编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
+     * 你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
+     * 此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
+     * @param s
+     * @return
+     */
+    public static String decodeString(String s) {
+        int index = 0;
+        LinkedList<String> stk = new LinkedList<String>();
+        while (index<s.length()){
+            char ch = s.charAt(index);
+            if(Character.isDigit(ch)){
+                StringBuilder sb = new StringBuilder();
+                while (Character.isDigit(ch)){
+                    sb.append(ch);
+                    ch = s.charAt(++index);
+                }
+                stk.addLast(sb.toString());
+            }else if(ch=='['||Character.isLetter(ch)){
+                stk.addLast(String.valueOf(ch));
+                index++;
+            }else {
+                List<String> list = new ArrayList<>();
+                while (!"[".equals(stk.peekLast())){
+                    list.add(stk.removeLast());
+                }
+                Collections.reverse(list);
+                stk.removeLast();
+                int times = Integer.valueOf(stk.removeLast());
+                StringBuilder sb = new StringBuilder();
+                for (String s1 : list) {
+                    sb.append(s1);
+                }
+                String curString = sb.toString();
+                for (int i = 1; i < times; i++) {
+                    sb.append(curString);
+                }
+                stk.addLast(sb.toString());
+                index++;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String s1 : stk) {
+            result.append(s1);
+        }
+        return result.toString();
 
     }
 }
