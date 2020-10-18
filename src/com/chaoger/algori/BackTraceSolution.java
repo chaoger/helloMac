742f06d53865f129d3afd08d1a373f38256c7461
leiyuchao <leiyuchao@corp.netease.com>
2020-10-18 18:20:17 +0800

    add 10-21 回溯+二叉搜索 两题

--- src/com/chaoger/algori/BackTraceSolution.java
+++ src/com/chaoger/algori/BackTraceSolution.java
@@ -1,6 +1,7 @@
 package com.chaoger.algori;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,11 +9,33 @@ import java.util.List;
  */
 public class BackTraceSolution {
 
+    List<List<Integer>> output = new ArrayList<>();
+
+    boolean[] visited;
 
     /**
      * 全排列:给定一个 没有重复 数字的序列，返回其所有可能的全排列。
      */
     public List<List<Integer>> permute(int[] nums) {
+        visited = new boolean[nums.length];
+        backTracePermute(new ArrayList<>(),nums);
+        return output;
+    }
+
+    public void backTracePermute(List<Integer> list,int[] nums){
+        if(list.size()==nums.length){
+            output.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(!visited[i]){
+                list.add(nums[i]);
+                visited[i] = true;
+                backTracePermute(list,nums);
+                list.remove(list.size()-1);
+                visited[i] = false;
+            }
+        }
 
     }
 }
