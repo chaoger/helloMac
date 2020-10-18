ed78fabf5a33c5b687aae7c4a9ed98b65d4353d1
leiyuchao <leiyuchao@corp.netease.com>
2020-10-18 18:36:12 +0800

    add 10-22 双指针两题

--- src/com/chaoger/algori/DoublePointSolution.java
+++ src/com/chaoger/algori/DoublePointSolution.java
@@ -5,11 +5,67 @@ package com.chaoger.algori;
  */
 public class DoublePointSolution {
 
-    //环形链表
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+    }
+
+    //
+
+    /**
+     * 环形链表:给定一个链表，判断链表是否有环
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode first = head;
+        while (first!=null&&first.next!=null){
+            slow = slow.next;
+            first = first.next.next;
+
+            if(slow==first){
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
-    //环形链表ii
+
+    /**
+     * 环形链表ii:给定一个链表，返回链表开始入环的第一个节点
+     * @param head
+     * @return
+     */
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode first = head;
+        while (first!=null&&first.next!=null){
+            slow = slow.next;
+            first = first.next.next;
+
+            if(slow==first){
+                break;
+            }
+        }
+        if(first==null||first.next==null){
+            return null;
+        }
+        slow = head;
+        while (slow!=first){
+            slow = slow.next;
+            first = first.next;
+        }
+
+        return slow;
+
+    }
 
 
 
