f9620cb5a61d732af3b6c84e142826b0a69fdd3a
leiyuchao <leiyuchao@corp.netease.com>
2020-10-23 18:18:21 +0800

    add 动态规划 2题

--- src/com/chaoger/algori/DpSolution.java
+++ src/com/chaoger/algori/DpSolution.java
@@ -32,10 +32,40 @@ public class DpSolution {
 
 
     /**
-     * 编辑距离
+     * 编辑距离:给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+     * 你可以对一个单词进行如下三种操作：
+     * 插入一个字符
+     * 删除一个字符
+     * 替换一个字符
      * @return
      */
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 1; i <=m; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i <=n; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <=m; i++) {
+            for (int j = 1; j <=n ; j++) {
+                int left = dp[i-1][j]+1;
+                int down = dp[i][j-1]+1;
+                int left_down = dp[i-1][j-1];
+                if(word1.charAt(i-1)!=word2.charAt(j-1)){
+                    left_down++;
 
+                }
+                dp[i][j] = Math.min(left_down,Math.min(left,down));
+
+            }
+        }
+        return dp[m][n];
+
+
+    }
 
 
     /**
@@ -342,8 +372,39 @@ public class DpSolution {
 
 
     /**
-     * 目标和
+     * 目标和:给定一个非负整数数组，a1, a2, ..., an, 和一个目标数，
+     * S。现在你有两个符号 + 和 -。对于数组中的任意一个整数，你都可以从 + 或 -中选择一个符号添加在前面。
+     *
+     * 返回可以使最终数组和为目标数 S 的所有添加符号的方法数。
      * @return
      */
+    public int findTargetSumWays(int[] nums, int S) {
+
+        //转换为背包问题
+        int sum = 0;
+        for (int num : nums) {
+            sum+=num;
+        }
+        if(S>sum){
+            return 0;
+        }
+        if((sum + S)%2==1){
+            return 0;
+        }
+
+        int target = (sum + S)/2;
+
+        int dp[] = new int[target+1];
+        dp[0] =1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = target; j >=nums[i]; j--) {
+                dp[j] = dp[j]+dp[j-nums[i]];
+            }
+        }
+
+        return dp[target];
+
+
+    }
 
 }
