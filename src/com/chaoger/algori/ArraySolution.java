e9699dfed902d6684c75073a704d71021796664c
leiyuchao <leiyuchao@corp.netease.com>
2020-10-24 18:56:24 +0800

    add 10-31 数组 2题

--- src/com/chaoger/algori/ArraySolution.java
+++ src/com/chaoger/algori/ArraySolution.java
@@ -31,15 +31,39 @@ public class ArraySolution {
     /**
      * 寻找两个正序数组的中位数:给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
      * 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
-     * @param A
-     * @param B
+     * @param nums1
+     * @param nums2
      * @return
      */
-    public double findMedianSortedArrays(int[] A, int[] B) {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        // TODO: 2020-09-24
-        return 0;
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = (n + m + 1) / 2;
+        int right = (n + m + 2) / 2;
 
+        return (getKth(nums1, 0, n-1, nums2, 0, m-1, left)
+                +getKth(nums1, 0, n-1, nums2, 0, m-1, right))*0.5;
+    }
+
+    private int getKth(int[] nums1,int start1,int end1,int[] nums2,int start2,int end2,int k){
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+        //让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
+        if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        if (len1 == 0) return nums2[start2 + k - 1];
+
+        if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+
+
+        int i = start1 + Math.min(len1, k / 2) - 1;
+        int j = start2 + Math.min(len2, k / 2) - 1;
+
+        if(nums1[i]>nums2[j]){
+            return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+        }else {
+            return getKth(nums1, i+1, end1, nums2, start2, end2, k - (i - start1 + 1));
+        }
     }
 
 
@@ -608,12 +632,62 @@ public class ArraySolution {
 
 
 
-    //
+
     /**
-     * 最大矩形
-     * @param heights
+     * 最大矩形:给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+     * @param matrix
      * @return
      */
+    public int maximalRectangle(char[][] matrix) {
+        int m = matrix.length;
+        if(m==0) return 0;
+        int n = matrix[0].length;
+        int[] height = new int[n];
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int res = 0;
+
+        Arrays.fill(right,n);
+
+        for (int i = 0; i < m; i++) {
+            int cur_left = 0;
+            int cur_right = n;
+            for (int j = 0; j < n; j++) {
+                if(matrix[i][j]=='1'){
+                    height[j]++;
+                }else {
+                    height[j] = 0;
+                }
+            }
+
+            for (int j = 0; j < n; j++) {
+                if(matrix[i][j]=='1'){
+                    left[j] = Math.max(left[j],cur_left);
+                }else {
+                    left[j] = 0;
+                    cur_left = j+1;
+                }
+            }
+
+            for (int j = n-1; j >=0; j--) {
+                if(matrix[i][j]=='1'){
+                    right[j] = Math.min(right[j],cur_right);
+                }else {
+                    right[j] = n;
+                    cur_right = j;
+                }
+            }
+
+            for (int j = 0; j <n; j++) {
+                res = Math.max(res,height[j]*(right[j]-left[j]));
+            }
+
+        }
+        return res;
+
+    }
+
+
 
 
     /**
